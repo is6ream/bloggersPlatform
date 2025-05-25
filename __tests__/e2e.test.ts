0a4995183cf4cc675433a191c2aab9b6882da215
is6ream <dnlils10@icloud.com>
2025-05-25 21:06:10 +0500

    остановился на реализации тестирования put

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -21,32 +21,6 @@ describe('/videos', () => {
             .expect(404)
     })
 
-    it('should create course with correct input data', async () => {
-
-        const res = await request(app)
-            .post('/hometask_01/api/videos')
-            .send({
-                title: 'string',
-                author: 'string',
-                availableResolutions: [
-                    "P144"
-                ]
-            })
-
-        const createdCourse = res.body
-
-        expect(createdCourse).toEqual({
-            id: expect.any(Number),
-            title: 'string',
-            author: 'string',
-            canBeDownloaded: true,
-            minAgeRestriction: null,
-            createdAt: expect.any(String),
-            publicationDate: expect.any(String),
-            availableResolutions: ["P144"]
-        })
-    })
-
     it('should not create course with incorrect input data', async () => {
 
         const res = await request(app)
@@ -60,6 +34,74 @@ describe('/videos', () => {
             })
             .expect(400)
     })
+
+
+    let createdCourse: any = null;
+    it('should create course with correct input data', async () => {
+
+        const res = await request(app)
+            .post('/hometask_01/api/videos')
+            .send({
+                title: 'string',
+                author: 'string',
+                availableResolutions: [
+                    "P144"
+                ]
+            })
+
+        createdCourse = res.body
+
+        expect(createdCourse).toEqual({
+            id: expect.any(Number),
+            title: 'string',
+            author: 'string',
+            canBeDownloaded: true,
+            minAgeRestriction: null,
+            createdAt: expect.any(String),
+            publicationDate: expect.any(String),
+            availableResolutions: ["P144"]
+        })
+
+        await request(app)
+            .get('/hometask_01/api/videos')
+            .expect(200, [createdCourse])
+
+    })
+
+    it('should not update course with incorrect input data', async () => {
+
+        interface DataForUpdating {
+
+            title: string,
+            author: string,
+            canBeDownloaded: boolean,
+            minAgeRestriction: number | null,
+            createdAt: string,
+            publicationDate: string,
+            availableResolutions: string[]
+
+        }
+
+        const dataForUpdating: DataForUpdating = {
+            title: '',
+            author: 'a',
+            canBeDownloaded: true,
+            minAgeRestriction: null,
+            createdAt: new Date().toISOString(),
+            publicationDate: new Date().toISOString(),
+            availableResolutions: [
+                'P1080'
+            ]
+
+        }
+        await request(app)
+            .put('hometask_01/api/videos/' + createdCourse.id)
+            .send(dataForUpdating) //выдает invalid URL
+            .expect(400)
+    })
+
+
+
 
 
 })
