682384d2af68b664034483b2565c0c58d7fcb77a
is6ream <dnlils10@icloud.com>
2025-05-25 14:19:59 +0500

    написал первый тест

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -2,10 +2,22 @@ import { app } from '../src/app'
 import request from "supertest";
 
 
+
+
+
 describe("GET /", () => {
     it("should return 'Hello world'", async () => {
         const res = await request(app).get("/");
         expect(res.status).toBe(200);
         expect(res.text).toBe("Hello world!")
+    })
+})
+
+
+describe('/videos', () => {
+    it('should return 200 and emty array', async () => {
+        await request(app)
+            .get('/')
+            .expect(200)
     })
 })
