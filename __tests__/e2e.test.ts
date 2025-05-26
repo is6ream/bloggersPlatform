957a0a80fda22591e9b8793004c53aec919a1fce
is6ream <dnlils10@icloud.com>
2025-05-26 21:28:07 +0500

    закончил с тестами

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -1,7 +1,9 @@
 import { app } from '../src/app'
 import request from "supertest";
 import { db } from '../src/db';
-
+import { correctDataForUpdating, incorrectDataForUpdating, DataForUpdating } from './test-helper';
+import { create } from 'domain';
+import { response } from 'express';
 
 
 describe('/videos', () => {
@@ -37,6 +39,9 @@ describe('/videos', () => {
 
 
     let createdCourse: any = null;
+    let createdCourse2: any = null
+
+
     it('should create course with correct input data', async () => {
 
         const res = await request(app)
@@ -68,36 +73,94 @@ describe('/videos', () => {
 
     })
 
-    it('should not update course with incorrect input data', async () => {
+    it('should create one more course', async () => {
+        const res = await request(app)
+            .post('/hometask_01/api/videos')
+            .send({
+                title: 't1',
+                author: 'a1',
+                availableResolutions: [
+                    "P144"
+                ]
+            })
 
-        interface DataForUpdating {
+        createdCourse2 = res.body
 
-            title: string,
-            author: string,
-            canBeDownloaded: boolean,
-            minAgeRestriction: number | null,
-            createdAt: string,
-            publicationDate: string,
-            availableResolutions: string[]
-
-        }
-
-        const dataForUpdating: DataForUpdating = {
-            title: '',
-            author: 'a',
+        expect(createdCourse2).toEqual({
+            id: expect.any(Number),
+            title: 't1',
+            author: 'a1',
             canBeDownloaded: true,
             minAgeRestriction: null,
-            createdAt: new Date().toISOString(),
-            publicationDate: new Date().toISOString(),
-            availableResolutions: [
-                'P1080'
-            ]
+            createdAt: expect.any(String),
+            publicationDate: expect.any(String),
+            availableResolutions: ["P144"]
+        })
 
-        }
         await request(app)
-            .put('hometask_01/api/videos/' + createdCourse.id)
-            .send(dataForUpdating) //выдает invalid URL
+            .get('/hometask_01/api/videos')
+            .expect(200, [createdCourse, createdCourse2])
+    })
+
+    it('should not update course with incorrect input data', async () => {
+        await request(app)
+            .put('/hometask_01/api/videos/' + createdCourse.id)
+            .send(incorrectDataForUpdating) //выдает invalid URL
             .expect(400)
+
+        await request(app)
+            .get('/hometask_01/api/videos/' + createdCourse.id)
+            .expect(200, createdCourse)
+    })
+
+    it('should not update course that not exist', async () => {
+        await request(app)
+            .put('/hometask_01/api/videos/12321')
+            .send(correctDataForUpdating) //выдает invalid URL
+            .expect(404)
+    })
+
+    it('should  update course with correct input data', async () => {
+        await request(app)
+            .put('/hometask_01/api/videos/' + createdCourse.id)
+            .send(correctDataForUpdating)
+            .expect(204)
+
+
+
+        const response = await request(app).
+            get('/hometask_01/api/videos/' + createdCourse.id)
+        expect(200)
+
+
+        expect(response.body).toEqual(expect.objectContaining({
+            ...correctDataForUpdating,
+            id: createdCourse.id,
+            createdAt: expect.any(String),
+            publicationDate: expect.any(String)
+        }))
+    })
+
+    it('should delete both courses', async () => {
+        await request(app)
+            .delete('/hometask_01/api/videos/' + createdCourse.id)
+            .expect(204)
+
+        await request(app)
+            .get('/hometask_01/api/videos/' + createdCourse.id)
+            .expect(404)
+
+        await request(app)
+            .delete('/hometask_01/api/videos/' + createdCourse2.id)
+            .expect(204)
+
+        await request(app)
+            .get('/hometask_01/api/videos/' + createdCourse2.id)
+            .expect(404)
+
+        await request(app)
+            .get('/hometask_01/api/videos')
+            .expect(200, [])
     })
 
 
---
+++ __tests__/test-helper.ts
@@ -0,0 +1,41 @@
+
+//вспомогателньные данные для тестов
+
+
+export interface DataForUpdating {
+
+    title: string,
+    author: string,
+    canBeDownloaded: boolean,
+    minAgeRestriction: number | null,
+    createdAt: string,
+    publicationDate: string,
+    availableResolutions: string[]
+
+}
+
+export const incorrectDataForUpdating: DataForUpdating = {
+    title: '',
+    author: 'a',
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: new Date().toISOString(),
+    publicationDate: new Date().toISOString(),
+    availableResolutions: [
+        'P1080'
+    ]
+
+}
+
+export const correctDataForUpdating: DataForUpdating = {
+
+    title: 'good title',
+    author: 'author1',
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: new Date().toISOString(),
+    publicationDate: new Date().toISOString(),
+    availableResolutions: [
+        'P720'
+    ]
+}
