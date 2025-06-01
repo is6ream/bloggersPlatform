dbe472df6f98df3c95f37223c7144ac32116cff8
is6ream <dnlils10@icloud.com>
2025-06-01 17:25:41 +0500

    импортировал пострепозиторий в хендлер всех постов

--- src/posts/handlers/getAllpostsHandler.ts
+++ src/posts/handlers/getAllpostsHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { HttpStatus } from "../../core/types";
+import { postRepository } from "../repositories/postRepository";
 
 export function getAllPostsHandler(req: Request, res: Response) {
   const posts = postRepository.findAll();
