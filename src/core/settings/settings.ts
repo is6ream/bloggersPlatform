affc62118ac4188fdcd253ac60233dd29ae3fe76
is6ream <dnlils10@icloud.com>
2025-06-03 07:27:49 +0500

    настроил файл settings

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,4 +1,5 @@
 export const SETTINGS = {
   PORT: process.env.PORT || 5001,
-  MONGO_URL: process.env.MONGO_URL,
+  MONGO_URL: process.env.MONGO_URL || "mongodb://localhost:27017/",
+  DB_NAME: process.env.DB_NAME || "ed-back-lessons-platform",
 };
