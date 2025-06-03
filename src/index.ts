4cfcc2957a5eff43f912c9a1ebd199bfcf7e6123
is6ream <dnlils10@icloud.com>
2025-06-03 07:19:41 +0500

    начал работать с mongo, добавил папку settings в core

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { app } from "./setup-app";
+import express from 'express';
+import { setupApp } from "./setup-app";
+ 
 
+
+
+const bootStrap = async () => {
+  const app = express();
+  setupApp(app)
 const PORT = process.env.PORT || 5001;
+await runDB()
+}
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}`);
