a35e1a0a6ca8a6185b10cc9a2f8dacaed3574be5
is6ream <dnlils10@icloud.com>
2025-06-05 07:42:41 +0500

    сделал createPostHandler асинхронным

--- src/posts/handlers/createPostHandler.ts
+++ src/posts/handlers/createPostHandler.ts
@@ -3,14 +3,15 @@ import { postRepository } from "../repositories/postRepository";
 import { PostType } from "../types/posts-types";
 import { HttpStatus } from "../../core/types";
 import { blogCollection } from "../../db/mongo.db";
+import { blogsRepository } from "../../blogs/repositories/blogs.repository";
 
 function generateNumericId(length = 10) {
   const randomNumber = Math.floor(Math.random() * Math.pow(10, length));
   return randomNumber.toString().padStart(length, "0");
 }
 
-export function createPostHandler(req: Request, res: Response) {
-  const foundBlog = blogCollection.findOne({ _id: req.body.id });
+export async function createPostHandler(req: Request, res: Response) {
+  const foundBlog = await blogsRepository.findById(req.body.blogId);
 
   if (!foundBlog) {
     res.status(HttpStatus.NotFound).send("Blog not found");
