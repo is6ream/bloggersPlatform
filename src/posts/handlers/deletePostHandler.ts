1193e0982dbc624475559c4e2b57d0116c58c127
is6ream <dnlils10@icloud.com>
2025-06-05 07:43:51 +0500

    сделал deletePostHandler асинхронным

--- src/posts/handlers/deletePostHandler.ts
+++ src/posts/handlers/deletePostHandler.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import { HttpStatus } from "../../core/types";
 import { postRepository } from "../repositories/postRepository";
 
-export function deletePostHandler(req: Request, res: Response) {
+export async function deletePostHandler(req: Request, res: Response) {
   const id = req.params.id;
-  postRepository.delete(id);
+  await postRepository.delete(id);
   res.status(HttpStatus.NoContent).send();
 }
