49cd6d162b1ac8ae2faa5c2da16e1c9099622a27
is6ream <dnlils10@icloud.com>
2025-06-05 07:44:22 +0500

    сделал findPostHandler асинхронным

--- src/posts/handlers/findPostHandler.ts
+++ src/posts/handlers/findPostHandler.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import { postRepository } from "../repositories/postRepository";
 import { HttpStatus } from "../../core/types";
 
-export function findPostHandler(req: Request, res: Response) {
-  const post = postRepository.findById(req.params.id);
+export async function findPostHandler(req: Request, res: Response) {
+  const post = await postRepository.findById(req.params.id);
   if (!post) {
     res.status(HttpStatus.NotFound).send("Post not found!");
   }
