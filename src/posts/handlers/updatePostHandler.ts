d232806d6e399d88988f83019e9f4d38f2461b5d
is6ream <dnlils10@icloud.com>
2025-06-05 07:45:41 +0500

    сделал updatePostHandler асинхронным

--- src/posts/handlers/updatePostHandler.ts
+++ src/posts/handlers/updatePostHandler.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { postRepository } from "../repositories/postRepository";
 import { HttpStatus } from "../../core/types";
 
-export function updatePostHandler(req: Request, res: Response) {
-  postRepository.update(req.params.id, req.body);
+export async function updatePostHandler(req: Request, res: Response) {
+  await postRepository.update(req.params.id, req.body);
   res.status(HttpStatus.NoContent).send();
 }
