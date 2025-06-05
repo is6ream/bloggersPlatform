e0e40dd532ef767e326772b46ae4640e3a52f00e
is6ream <dnlils10@icloud.com>
2025-06-05 07:45:10 +0500

    сделал getAllPostHandler асинхронным

--- src/posts/handlers/getAllpostsHandler.ts
+++ src/posts/handlers/getAllpostsHandler.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { HttpStatus } from "../../core/types";
 import { postRepository } from "../repositories/postRepository";
 
-export function getAllPostsHandler(req: Request, res: Response) {
-  const posts = postRepository.findAll();
+export async function getAllPostsHandler(req: Request, res: Response) {
+  const posts = await postRepository.findAll();
   res.status(HttpStatus.Ok).json(posts);
 }
