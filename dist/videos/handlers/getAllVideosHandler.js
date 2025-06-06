7846ab3ffa1b6d721ec3adc88f1c6f6a60aea96b
is6ream <dnlils10@icloud.com>
2025-06-06 15:06:25 +0500

    удалил .env и dist из репозитория

--- dist/videos/handlers/getAllVideosHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllVideosHandler = getAllVideosHandler;
-const db_1 = require("../../db");
-function getAllVideosHandler(req, res) {
-  const videos = db_1.db.videos;
-  res.status(200).json(videos);
-}
