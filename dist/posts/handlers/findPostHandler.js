f78cf115642ce115f42f69efbe936ee0a5f47964
is6ream <dnlils10@icloud.com>
2025-06-07 15:26:00 +0500

    добавил try/catch в обработчик маршрута findPost

--- dist/posts/handlers/findPostHandler.js
+++ dist/posts/handlers/findPostHandler.js
@@ -14,10 +14,18 @@ const postRepository_1 = require("../repositories/postRepository");
 const types_1 = require("../../core/types");
 function findPostHandler(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        const post = yield postRepository_1.postRepository.findById(req.params.id);
-        if (!post) {
-            res.status(types_1.HttpStatus.NotFound).send("Post not found!");
+        try {
+            const post = yield postRepository_1.postRepository.findById(req.params.id);
+            if (!post) {
+                res.status(types_1.HttpStatus.NotFound).send("Post not found!");
+            }
+            res.status(types_1.HttpStatus.Ok).send(post);
+            return;
         }
-        res.status(types_1.HttpStatus.Ok).send(post);
+        catch (error) {
+            console.log(error);
+            res.sendStatus(types_1.HttpStatus.InternalServerError);
+            return;
+        }
     });
 }
--- src/posts/handlers/findPostHandler.ts
+++ src/posts/handlers/findPostHandler.ts
@@ -3,9 +3,16 @@ import { postRepository } from "../repositories/postRepository";
 import { HttpStatus } from "../../core/types";
 
 export async function findPostHandler(req: Request, res: Response) {
-  const post = await postRepository.findById(req.params.id);
-  if (!post) {
-    res.status(HttpStatus.NotFound).send("Post not found!");
+  try {
+    const post = await postRepository.findById(req.params.id);
+    if (!post) {
+      res.status(HttpStatus.NotFound).send("Post not found!");
+    }
+    res.status(HttpStatus.Ok).send(post);
+    return;
+  } catch (error: unknown) {
+    console.log(error);
+    res.sendStatus(HttpStatus.InternalServerError);
+    return;
   }
-  res.status(HttpStatus.Ok).send(post);
 }
