5f8c0f1a5b51cf13116e2b8095ea25faa60d8595
is6ream <dnlils10@icloud.com>
2025-06-12 15:05:56 +0500

    закончил с функцией-маппером для хендлера блогов

--- src/blogs/routes/mappers/map-to-blog-list-paginated-output.util.ts
+++ src/blogs/routes/mappers/map-to-blog-list-paginated-output.util.ts
@@ -4,4 +4,27 @@ import { ResourceType } from "../../../core/types/resource-type";
 import { BlogListPaginatedOutput } from "../output/blog-list-paginated.output";
 import { BlogDataOutput } from "../output/blog-data.output";
 
-export function 
+export function mapToBlogListPaginatedOutput(
+  blogs: WithId<BlogType>[],
+  meta: { pageNumber: number; pageSize: number; totalCount: number }
+): BlogListPaginatedOutput {
+  return {
+    meta: {
+      page: meta.pageNumber,
+      pageSize: meta.pageSize,
+      pageCount: Math.ceil(meta.totalCount / meta.pageSize),
+      totalCount: meta.totalCount,
+    },
+    data: blogs.map(
+      (blog): BlogDataOutput => ({
+        type: ResourceType.Blogs,
+        id: blog._id.toString(),
+        name: blog.name,
+        description: blog.description,
+        websiteUrl: blog.websiteUrl,
+        createdAt: blog.createdAt,
+        isMembership: blog.isMembership,
+      }),
+    ),
+  };
+}
