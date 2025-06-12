10f839e66c8466de8f205d03786ff1d428f46dc3
is6ream <dnlils10@icloud.com>
2025-06-12 14:59:00 +0500

    начал работу над функцией, которая будет мапить данные в хендлере поиска всех блогов

--- src/blogs/routes/mappers/map-to-blog-list-paginated-output.util.ts
+++ src/blogs/routes/mappers/map-to-blog-list-paginated-output.util.ts
@@ -2,3 +2,6 @@ import { WithId } from "mongodb";
 import { BlogType } from "../../types/blogs-types";
 import { ResourceType } from "../../../core/types/resource-type";
 import { BlogListPaginatedOutput } from "../output/blog-list-paginated.output";
+import { BlogDataOutput } from "../output/blog-data.output";
+
+export function 
