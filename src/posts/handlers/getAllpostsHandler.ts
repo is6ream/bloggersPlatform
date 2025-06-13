e13fabae4ee58265c0b50b113e2d54594ab3caad
is6ream <dnlils10@icloud.com>
2025-06-13 11:46:06 +0500

    прописал тип PostDataOutput для маппинга постов

--- src/posts/handlers/getAllpostsHandler.ts
+++ src/posts/handlers/getAllpostsHandler.ts
@@ -2,13 +2,16 @@ import { Request, Response } from "express";
 import { HttpStatus } from "../../core/http-statuses";
 import { postRepository } from "../repositories/postRepository";
 import { setDefaultPaginationIfNotExist } from "../../core/helpers/set-default-sort-and-pagination";
+import { postsService } from "../application/post.service";
 
 export async function getAllPostsHandler(req: Request, res: Response) {
   try {
     const queryInput = setDefaultPaginationIfNotExist(req.query);
 
-    const { items, totalCount } = await 
-  }
+    const { items, totalCount } = await postsService.findMany(queryInput)
+
+    const postsListOutput = 
+  } d
   
   const posts = await postRepository.findAll();
   res.status(HttpStatus.Ok).json(posts);
