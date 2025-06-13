5064aec525e0b57f2aa51fe094a86155588b6a39
is6ream <dnlils10@icloud.com>
2025-06-13 17:32:22 +0500

    удалил папку dist

--- dist/core/settings/settings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SETTINGS = void 0;
-exports.SETTINGS = {
-    PORT: process.env.PORT || 5001,
-    MONGO_URL: process.env.MONGO_URL ||
-        "mongodb+srv://admin:admin@cluster0.nm5nplv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    DB_NAME: process.env.DB_NAME || "ed-back-lessons-platform",
-};
--- dist/core/types/resource-type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResourceType = void 0;
-var ResourceType;
-(function (ResourceType) {
-    ResourceType["Blogs"] = "blogs";
-    ResourceType["Posts"] = "posts";
-})(ResourceType || (exports.ResourceType = ResourceType = {}));
