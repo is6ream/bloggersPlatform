02fba485119b77b57b2daabd2ab5ff370725a9e9
is6ream <dnlils10@icloud.com>
2025-06-13 11:58:22 +0500

    прописал фунцию-маппер для хендлера блогов

--- src/posts/mappers/map-to-post-list-paginated-output.util.ts
+++ src/posts/mappers/map-to-post-list-paginated-output.util.ts
@@ -1,3 +1,31 @@
+import { PostDataOutput } from "./../output/post-data-output";
 import { WithId } from "mongodb";
 import { PostType } from "../types/posts-types";
 import { ResourceType } from "../../core/types/resource-type";
+import { PostListPaginatedOutput } from "../output/post-list-paginated.output";
+
+export function mapToBlogListPaginatedOutput(
+  posts: WithId<PostType>[],
+  meta: { pageNumber: number; pageSize: number; totalCount: number }
+): PostListPaginatedOutput {
+  return {
+    meta: {
+      page: meta.pageNumber,
+      pageSize: meta.pageSize,
+      pageCount: Math.ceil(meta.totalCount / meta.pageSize),
+      totalCount: meta.totalCount,
+    },
+    data: posts.map(
+      (post): PostDataOutput => ({
+        type: ResourceType.Posts,
+        id: post._id.toString(),
+        title: post.title,
+        shortDescription: post.shortDescription,
+        content: post.content,
+        blogId: post.blogId,
+        blogName: post.blogName,
+        createdAt: post.createdAt,
+      })
+    ),
+  };
+}
