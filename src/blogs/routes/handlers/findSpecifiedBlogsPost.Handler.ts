c0ed8b79747aea1719db0a40357c311cb2bf123f
is6ream <dnlils10@icloud.com>
2025-06-15 13:02:26 +0500

    начал реализацию пути поиска постов для специального блога

--- src/blogs/routes/handlers/findSpecifiedBlogsPost.Handler.ts
+++ src/blogs/routes/handlers/findSpecifiedBlogsPost.Handler.ts
@@ -3,3 +3,10 @@ import { blogsRepository } from "../../repositories/blogs.repository";
 import { HttpStatus } from "../../../core/http-statuses";
 import { BlogType } from "../../types/blogs-types";
 import { createErrorMessages } from "../../../core/error.utils";
+import { blogsService } from "../../application/blogs.service";
+
+export async function findPostForSpecifiedBlog(req: Request, res: Response) {
+    try {
+        const blog = await blogsService.findByIdOrFail(req.params.id)
+    }
+}
