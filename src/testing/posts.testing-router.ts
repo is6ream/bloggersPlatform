14e8a650aa95b8334c3f8dfa1c4856692cadcc4a
is6ream <dnlils10@icloud.com>
2025-06-17 19:48:25 +0500

    прописал общий обработчик для удаления данных из бд

--- src/testing/posts.testing-router.ts
+++ src/testing/posts.testing-router.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { deleteAllPosts } from "../posts/handlers/deleteAllPostsHandler";
 import { deleteAllBlogs } from "../blogs/routes/handlers/deleteAllBlogsHandler";
-
+import { HttpStatus } from "../core/http-statuses";
+import { Request, Response } from "express";
 export const testingRouter = Router();
 
-testingRouter
-  .delete("/all-data", deleteAllBlogs)
+async function deleteAllData(req: Request, res: Response) {
+  try {
+    await deleteAllBlogs(req, res);
+    await deleteAllPosts(req, res);
 
-  .delete("/all-data", deleteAllPosts);
+    res.sendStatus(HttpStatus.NoContent);
+  } catch (error: unknown) {
+    console.log("Error deleting all data: ", error);
+    res.sendStatus(HttpStatus.InternalServerError);
+  }
+}
+
+testingRouter.delete("/all-data", deleteAllData);
