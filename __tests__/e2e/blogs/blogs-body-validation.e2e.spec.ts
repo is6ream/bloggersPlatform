9be3955be74357a4de9d6c1fbcf147f177d9eb76
is6ream <dnlils10@icloud.com>
2025-06-21 20:52:19 +0500

    прописал логику подключения и очистки базы данных перед выполнением тестов

--- __tests__/e2e/blogs/blogs-body-validation.e2e.spec.ts
+++ __tests__/e2e/blogs/blogs-body-validation.e2e.spec.ts
@@ -3,6 +3,8 @@ import express from "express";
 import { BlogAttributes } from "./types";
 import { getBlogDto } from "../../utils/blogs/get-blog-dto";
 import { generateBasicAuthToken } from "../../utils/posts/generate-admin-auth-token";
+import { runDB } from "../../../src/db/mongo.db";
+import { clearDb } from "../../utils/posts/clear-db";
 
 describe("Blog API body validation check", () => {
   const app = express();
@@ -11,4 +13,11 @@ describe("Blog API body validation check", () => {
   const correctTestBlogAttributes: BlogAttributes = getBlogDto();
 
   const adminToken = generateBasicAuthToken();
+
+  beforeAll(async () => {
+    await runDB(
+      "mongodb+srv://admin:admin@firstcluster.atxbolf.mongodb.net/?retryWrites=true&w=majority&appName=FirstCluster"
+    );
+    await clearDb(app);
+  });
 });
--- __tests__/utils/posts/clear-db.ts
+++ __tests__/utils/posts/clear-db.ts
@@ -2,8 +2,7 @@
 import request from "supertest";
 import { Express } from "express";
 import { TESTING_PATH } from "../../../src/core/paths";
-import { HttpStatus } from "../../../src/core/types";
-
+import { HttpStatus } from "../../../src/core/http-statuses";
 export async function clearDb(app: Express) {
   await request(app)
     .delete(`${TESTING_PATH}/all-data`)
