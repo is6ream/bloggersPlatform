69e580bc1c21f942831e525d879e754d0726ffcb
is6ream <dnlils10@icloud.com>
2025-06-22 12:55:18 +0500

    прописал роутер, в котором напрямую обращаюсь к коллекции

--- src/testing/deleteAllData.router.ts
+++ src/testing/deleteAllData.router.ts
@@ -1,22 +1,10 @@
 import { Router } from "express";
-import { deleteAllPosts } from "../posts/handlers/deleteAllPostsHandler";
-import { deleteAllBlogs } from "../blogs/routes/handlers/deleteAllBlogsHandler";
 import { HttpStatus } from "../core/http-statuses";
 import { Request, Response } from "express";
+import { blogCollection, postCollection } from "../db/mongo.db";
 export const testingRouter = Router();
 
-async function deleteAllData(req: Request, res: Response) {
-  try {
-    deleteAllBlogs;
-    deleteAllPosts;
-
-    res.sendStatus(HttpStatus.NoContent);
-    return;
-  } catch (error: unknown) {
-    console.log("Error deleting all data: ", error);
-    res.sendStatus(HttpStatus.InternalServerError);
-    return;
-  }
-}
-
-testingRouter.delete("/all-data", deleteAllData);
+testingRouter.delete("/all-data", async (req: Request, res: Response) => {
+  Promise.all([blogCollection.deleteMany(), postCollection.deleteMany()]);
+  res.sendStatus(HttpStatus.NoContent);
+});
