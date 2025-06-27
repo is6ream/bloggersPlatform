554f1aafee5a18789cb09b26798c1964aaf035c1
is6ream <dnlils10@icloud.com>
2025-06-27 07:45:56 +0500

    работаю над примером маппинга данных в репозитории

--- src/draft.ts
+++ src/draft.ts
@@ -1,3 +1,5 @@
+import { Db } from "mongodb";
+
 export interface CreateUserInpuDto {
   login: string;
   password: string;
@@ -11,6 +13,7 @@ export interface IUserDB {
   createdAt: Date;
 }
 
+//пример сервиса
 export const usersService = {
   async create(dto: CreateUserInpuDto): Promise<string> {
     const { login, email, password } = dto;
@@ -24,5 +27,70 @@ export const usersService = {
     };
     const newUserId = await usersRepository.create(newUser);
     return newUserId;
+  },
+};
+
+//пример хендлера
+async (req: RequestWithBody<CreateUserInputDto>, res: Response<IUserView>) => {
+  const { login, password, email } = req.body;
+
+  const userId = await usersService.create({ login, password, email }); //создаем с помощью dto
+  const newUser = await usersQwRepository.findById(userId); //далее получаем из query репозитория данные
+
+  return res.status(HttpStatuses.Created).send(newUser);
+};
+
+type VideoOutputModel = {
+  id: string;
+  title: string;
+  author: {
+    id: string;
+    name: string;
+  };
+};
+
+type DBVideo = {
+  _id: string;
+  title: string;
+  authorId: string;
+};
+
+type DBAuthor = {
+  _id: string;
+  firstName: string;
+  lastName: string;
+};
+
+const videoQueryRepo = {
+  getVideos(): VideoOutputModel[] {
+    const dbVideos: DBVideo[] = [];
+    const authors: DBAuthor[];
+    return dbVideos.map((dbVideo) => {
+      const author = authors.find((a) => a._id === dbVideo.authorId);
+
+      return {
+        id: dbVideo._id,
+        author: {
+          id: author?._id,
+          name: author?.firstName + " " + author?.lastName,
+        },
+        title: dbVideo.title,
+      };
+    });
+  },
+
+  getVideoById(id: string): VideoOutputModel {
+    const dbVideos: DBVideo = {
+      _id: "3334",
+      title: "sds",
+      authorId: "3232",
+    };
+    const author: DBAuthor = {
+      _id: "32",
+      lastName: "da",
+      firstName: "das",
+    };
+
+    
   },
 };
