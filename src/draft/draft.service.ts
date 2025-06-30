23d4db95d3178db4245248973cbc754d27ce18dc
is6ream <dnlils10@icloud.com>
2025-06-30 21:28:07 +0500

    прописал пример сервиса для создания user

--- src/draft/draft.service.ts
+++ src/draft/draft.service.ts
@@ -1,46 +1,17 @@
-import { ObjectId } from "mongodb";
-import bcrypt from "bcrypt";
-
-export type UserDBType = {
-  _id: ObjectId;
-  userName: string;
-  email: string;
-  passwordHash: string;
-  passwordSalt: string;
-  createdAt: Date;
-};
 export const usersService = {
-  async createUser(
-    login: string,
-    email: string,
-    password: string
-  ): Promise<UserDBType> {
-    const passwordSalt = await bcrypt.genSalt(10);
-    const passwordHash = await this._generateHash(password, passwordSalt);
+  async create(dto: any): Promise<string> {
+    const { login, password, email } = dto;
+    const passwordHash = await bcryptService.generateHash(password);
 
-    const newUser: UserDBType = {
-      _id: new ObjectId(),
-      userName: login,
+    const newUser = {
+      login,
       email,
       passwordHash,
-      passwordSalt,
-      createdAt: new Date(),
+      createdAt: new Date()
     };
-    return usersRepository.createUser(newUser);
-  },
 
-  async checkCredentials(loginOrEmail: string, password: string){
-    const user = await usersRepository.findByLoginOrEmail(loginOrEmail); //
-    if(!user) return false
-    const passwordHash = await this._generateHash(password, user.passwordSalt)
-    if(user.passwordHash !== passwordHash){
-        return false
-    } return true
-  }
+    const newUserId = await usersRepository.create(newUser);
 
-  async _generateHash(password: string, salt: string) {
-    const hash = await bcrypt.hash(password, salt);
-    console.log("hash" + hash);
-    return hash;
+    return newUserId;
   },
 };
