4d1a2e719d8f4fb2aed7f5325de3d9c75d216c32
is6ream <dnlils10@icloud.com>
2025-07-01 21:41:52 +0500

    убрал ненужную валидацию

--- src/auth/routes/routes.ts
+++ src/auth/routes/routes.ts
@@ -2,12 +2,10 @@ import { Router } from "express";
 import { authValidators } from "../middlewares/auth.validation";
 import { inputValidationResultMiddleware } from "../../core/middlewares/validation/input-validation-result.middleware";
 import { loginUserHandler } from "../handler/auth.userHandler";
-import { superAdminGuardMiddleware } from "../../core/middlewares/validation/super-admin.guard-middleware";
 export const authRouter = Router();
 
 authRouter.post(
   "/auth/login",
-  superAdminGuardMiddleware,
   authValidators,
   inputValidationResultMiddleware,
   loginUserHandler,
