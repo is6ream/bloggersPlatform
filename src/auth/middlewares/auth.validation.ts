30135bfcfc3ebaf0202fb5b675680473eb3be892
is6ream <dnlils10@icloud.com>
2025-07-01 20:14:25 +0500

    прописал валидацию для dto авторизации

--- src/auth/middlewares/auth.validation.ts
+++ src/auth/middlewares/auth.validation.ts
@@ -1,1 +1,15 @@
-import { body, bodyValidation } from "express-validator";
+import { body } from "express-validator";
+
+export const loginOrEmailValidator = body("loginOrEmail")
+  .exists()
+  .withMessage("field login or email is required")
+  .bail()
+  .isString()
+  .withMessage("field login or email must be a string");
+
+export const passwordValidation = body("password")
+  .exists()
+  .withMessage("field password is required")
+  .bail()
+  .isString()
+  .withMessage("field password must be a string");
