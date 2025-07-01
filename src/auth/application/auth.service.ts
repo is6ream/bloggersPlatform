e13323165b5da676f2146ed65f93121aaee3f0e0
is6ream <dnlils10@icloud.com>
2025-07-01 20:49:26 +0500

    прописал сервис для авторизационного эднпоинта

--- src/auth/application/auth.service.ts
+++ src/auth/application/auth.service.ts
@@ -1,8 +1,21 @@
-import { createAuthDto } from "../handler/loginUserHandler";
+import { CreateAuthDto } from "../handler/loginUserHandler";
+import bcrypt from "bcrypt";
 
 export const authService = {
-  async create(dto: createAuthDto): Promise<void> {
-    authRepository.create(dto);
-    return;
+  async create(loginOrEmail: string, password: string): Promise<void> {
+    const passwordSalt = await bcrypt.genSalt(10);
+    const passwordHash = await this._generateHash(password, passwordSalt);
+
+    const dto: CreateAuthDto = {
+      passwordHash,
+      loginOrEmail,
+    };
+
+    return authRepository.create(dto);
+  },
+
+  async _generateHash(password: string, salt: string) {
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
   },
 };
--- src/auth/handler/loginUserHandler.ts
+++ src/auth/handler/loginUserHandler.ts
@@ -1,17 +1,13 @@
 import { Request, Response } from "express";
 import { authService } from "../application/auth.service";
 
-export type createAuthDto = {
+export type CreateAuthDto = {
   loginOrEmail: string;
   passwordHash: string;
 };
 export async function loginUserHandler(req: Request, res: Response) {
   const { loginOrEmail, password } = req.body;
-  const passwordHash = await bcryptService.generateHash(password);
-  const dto: createAuthDto = {
-    loginOrEmail,
-    passwordHash,
-  };
-  const dataForResponse = await authService.create(dto);
+
+  const dataForResponse = await authService.create(req.body);
   res.status(204).send(dataForResponse);
 }
