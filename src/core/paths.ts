fd3b62156b4dc8a0802beb7e67f1b24db0c27a70
is6ream <dnlils10@icloud.com>
2025-07-01 21:36:39 +0500

    прописал роуты и подключил роутер

--- src/core/paths.ts
+++ src/core/paths.ts
@@ -1,4 +1,5 @@
-export const BLOGS_PATH = "/hometask_04/api/blogs";
-export const POSTS_PATH = "/hometask_04/api/posts";
-export const TESTING_PATH = "/hometask_04/api/testing";
-export const TESTING_HW_PATH = "/hometask_04/api/";
+export const BLOGS_PATH = "/hometask_05/api/blogs";
+export const POSTS_PATH = "/hometask_05/api/posts";
+export const TESTING_PATH = "/hometask_05/api/testing";
+export const TESTING_HW_PATH = "/hometask_05/api/";
+export const AUTH_PATH = "/hometask_05/api/";
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import { blogsRouter } from "./blogs/blogs.routes";
-import { BLOGS_PATH, POSTS_PATH, TESTING_PATH } from "./core/paths";
+import { AUTH_PATH, BLOGS_PATH, POSTS_PATH, TESTING_PATH } from "./core/paths";
 import { Express } from "express";
 import { postRouter } from "./posts/routes/routes";
 import { testingRouter } from "./testing/deleteAllData.router";
+import { authRouter } from "./auth/routes/routes";
 export const app = express();
 
 export const setupApp = (app: Express) => {
@@ -16,6 +17,7 @@ export const setupApp = (app: Express) => {
   app.use(BLOGS_PATH, blogsRouter);
   app.use(POSTS_PATH, postRouter);
   app.use(TESTING_PATH, testingRouter);
+  app.use(AUTH_PATH, authRouter);
 
   return app;
 };
