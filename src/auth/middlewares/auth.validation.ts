e3d36437a4949661b4555b5a2e6253f26b580969
is6ream <dnlils10@icloud.com>
2025-07-02 20:49:57 +0500

    реализовал валидацию данных на пути авторизации суперадмина

--- src/auth/middlewares/auth.validation.ts
+++ src/auth/middlewares/auth.validation.ts
@@ -1,24 +1,17 @@
 import { body } from "express-validator";
 import { authColletction } from "../../db/mongo.db";
 
+const combinedRegex = /^(?:[a-zA-Z0-9_-]*|[\w.-]+@([\w-]+\.)+[\w-]{2,4})$/;
+
 export const loginOrEmailValidator = body("loginOrEmail")
   .trim()
   .notEmpty()
   .withMessage("Требуется логин или email")
-  .custom(async (value, { req }) => {
-    const isEmail = value.includes("@");
-
-    const user = await authColletction.findOne({
-      $or: [{ login: value }, { email: value }],
-    });
-
-    if (!user) {
-      throw new Error("Неверные учетные данные");
-    }
-    req.user = user;
-    return true;
-  });
-//вопрос, как
+  .matches(combinedRegex)
+  .withMessage("input must be string or email")
+  .isLength({ min: 3, max: 20 })
+  .withMessage("More then 20 or 3");
+//вопрос, как реализовать эту аавторизацию
 
 export const passwordValidation = body("password")
   .exists()
--- src/testing/deleteAllData.router.ts
+++ src/testing/deleteAllData.router.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { HttpStatus } from "../core/http-statuses";
 import { Request, Response } from "express";
-import { blogCollection, postCollection } from "../db/mongo.db";
+import {
+  blogCollection,
+  postCollection,
+  authColletction,
+} from "../db/mongo.db";
 export const testingRouter = Router();
 
 testingRouter.delete("/all-data", async (req: Request, res: Response) => {
-  Promise.all([blogCollection.deleteMany(), postCollection.deleteMany()]);
+  Promise.all([
+    blogCollection.deleteMany(),
+    postCollection.deleteMany(),
+    authColletction.deleteMany(),
+  ]);
   res.sendStatus(HttpStatus.NoContent);
 });
