a0e419cf0bb32c04c3f059254112603fae0f5b04
is6ream <dnlils10@icloud.com>
2025-07-02 07:37:41 +0500

    изменил валидацию входных данных

--- src/auth/middlewares/auth.validation.ts
+++ src/auth/middlewares/auth.validation.ts
@@ -1,17 +1,31 @@
 import { body } from "express-validator";
+import { authColletction } from "../../db/mongo.db";
 
 export const loginOrEmailValidator = body("loginOrEmail")
-  .exists()
-  .withMessage("field login or email is required")
-  .bail()
-  .isString()
-  .withMessage("field login or email must be a string");
+  .trim()
+  .notEmpty()
+  .withMessage("Требуется логин или email")
+  .custom(async (value, { req }) => {
+    const isEmail = value.includes("@");
+
+    const user = await authColletction.findOne({
+      $or: [{ login: value }, { email: value }],
+    });
+
+    if (!user) {
+      throw new Error("Неверные учетные данные");
+    }
+    req.user = user;
+    return true;
+  });
+//вопрос, как
 
 export const passwordValidation = body("password")
   .exists()
   .withMessage("field password is required")
-  .bail()
   .isString()
-  .withMessage("field password must be a string");
+  .withMessage("field password must be a string")
+  .isLength({ min: 6, max: 20 })
+  .withMessage("more then 20 or 6");
 
 export const authValidators = [loginOrEmailValidator, passwordValidation];
