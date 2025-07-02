68480052aeb1538686330bf361f34cd92ed0166d
is6ream <dnlils10@icloud.com>
2025-07-02 20:59:01 +0500

    перекинул методы get в квери репо поста

---
+++ src/posts/repositories/postQueryRepository.ts
@@ -0,0 +1,69 @@
+export const postQueryRepository = {
+  async findAll(
+    queryDto: PostQueryInput,
+  ): Promise<{ items: WithId<PostType>[]; totalCount: number }> {
+    const { pageNumber, pageSize, sortBy, sortDirection, searchPostNameTerm } =
+      queryDto;
+    console.log(queryDto, "Тут логаю посты");
+
+    const skip = (pageNumber - 1) * pageSize;
+    const filter: any = {};
+
+    if (searchPostNameTerm) {
+      filter["name"] = { $regex: searchPostNameTerm, $options: "i" };
+    }
+    const items = await postCollection
+      .find(filter)
+      .sort({ [sortBy]: sortDirection })
+      .skip(skip)
+      .limit(+pageSize)
+      .toArray();
+
+    const totalCount = await postCollection.countDocuments(filter);
+
+    return { items, totalCount };
+  },
+
+  async findPostsByBlogId(
+    queryDto: PostQueryInput,
+    blogId: string,
+  ): Promise<{ items: WithId<PostType>[]; totalCount: number }> {
+    const { pageNumber, pageSize, sortBy, sortDirection, searchPostNameTerm } =
+      queryDto;
+    const skip = (pageNumber - 1) * pageSize;
+    const filter: Record<string, any> = {
+      blogId,
+    };
+
+    if (searchPostNameTerm) {
+      filter.name = { $regex: searchPostNameTerm, $options: "i" };
+    }
+
+    const items = await postCollection
+      .find(filter) //тут изменил на findOne, изменил обратно, т.к нужен массив
+      .sort({ [sortBy]: sortDirection })
+      .skip(skip)
+      .limit(+pageSize)
+      .toArray();
+
+    const totalCount = await postCollection.countDocuments(filter);
+
+    return { items, totalCount };
+  },
+
+  async findById(id: string): Promise<PostViewModel | null> {
+    const post = await postCollection.findOne({ _id: new ObjectId(id) });
+    if (!post) {
+      return null;
+    }
+    return {
+      id: post._id.toString(),
+      title: post.title,
+      shortDescription: post.shortDescription,
+      content: post.content,
+      blogId: post.blogId,
+      blogName: post.blogName,
+      createdAt: post.createdAt,
+    };
+  },
+};
