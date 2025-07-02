5c04b758a5ff8802cb1459d79a4e72391ec529d0
is6ream <dnlils10@icloud.com>
2025-07-02 21:56:24 +0500

    прописал авторизацию для сущности user

--- src/auth/middlewares/auth.validation.ts
+++ src/auth/middlewares/auth.validation.ts
@@ -5,6 +5,8 @@ const combinedRegex = /^(?:[a-zA-Z0-9_-]*|[\w.-]+@([\w-]+\.)+[\w-]{2,4})$/;
 export const loginOrEmailValidator = body("loginOrEmail")
   .trim()
   .notEmpty()
+  .isString()
+  .withMessage("input must be a string")
   .withMessage("Login or email is required")
   .matches(combinedRegex)
   .withMessage("input must be string or email")
--- src/users/middlewares/users.validation.ts
+++ src/users/middlewares/users.validation.ts
@@ -4,9 +4,24 @@ export const loginValidator = body("login")
   .trim()
   .notEmpty()
   .withMessage("Login is required")
-  .matches("^[a-zA-Z0-9_-]*$")
+  .matches(/^[a-zA-Z0-9_-]*$/)
   .withMessage("input must be a string")
   .isLength({ min: 3, max: 10 })
   .withMessage("More then 10 or 3");
 
-export 
+export const passwordValidation = body("password")
+  .exists()
+  .withMessage("field password is required")
+  .isString()
+  .withMessage("field password must be a string")
+  .isLength({ min: 6, max: 20 })
+  .withMessage("more then 20 or 6");
+
+export const emailValidation = body("email")
+  .exists()
+  .withMessage("field email is required")
+  .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+  .withMessage("input must be a email")
+  .isString()
+  .withMessage("field email must be a string");
+
