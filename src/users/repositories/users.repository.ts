6ee5672f0489306e893cdd64d306d5e58b7422eb
is6ream <dnlils10@icloud.com>
2025-07-03 20:31:06 +0500

    нужно создать usersCollection в дб

---
+++ src/users/repositories/users.repository.ts
@@ -0,0 +1,9 @@
+import { CreateUserDto } from "../input/create-user-dto";
+import {  UserViewModel } from "../types/user-types";
+import {}
+
+export const usersRepository = {
+  async create(newUser: CreateUserDto): Promise<UserViewModel> {
+
+  },
+};
