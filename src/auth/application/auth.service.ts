82df84c2444a12932c76e950d2690224d3c9b003
is6ream <dnlils10@icloud.com>
2025-07-16 22:37:24 +0500

    прописал authService

--- src/auth/application/auth.service.ts
+++ src/auth/application/auth.service.ts
@@ -1,39 +1,33 @@
-import bcrypt from "bcrypt";
-import { CreateAuthDto } from "../types/input/login-input.model";
 import { WithId } from "mongodb";
 import { UserDBType } from "../../users/input/create-user-dto";
 import { usersRepository } from "../../users/repositories/users.repository";
 import { ResultStatus } from "../../common/result/resultCode";
 import { Result } from "../../common/result/result.type";
 import { bcryptService } from "../adapters/bcrypt.service";
-import { HttpStatus } from "../../core/http-statuses";
+
 export const authService = {
-  async create(loginOrEmail: string, password: string): Promise<AuthResult> {
-    const passwordSalt = await bcrypt.genSalt(10);
-    const passwordHash = await this._generateHash(password, passwordSalt);
+  async loginUser(
+    loginOrEmail: string,
+    password: string,
+  ): Promise<Result<UserDBType | null>> {
+    const result = await this.checkUserCredentials(loginOrEmail, password);
+    if (result.status !== ResultStatus.Success)
+      return {
+        status: ResultStatus.Unauthorized,
+        errorMessage: "Unauthorized",
+        extensions: [{ field: "loginOrEmail", message: "Wrong credentials" }],
+        data: null,
+      };
 
-    const dto: CreateAuthDto = {
-      passwordHash,
-      loginOrEmail,
-      createdAt: new Date(),
+    return {
+      status: ResultStatus.Success,
+      extensions: [],
     };
-
-    const authId = await authRepository.create(dto);
-    const newAuth = await authQueryRepository.findById(authId);
-
-    return newAuth!;
-    //обратиться в репо, найти пользователя и проверить совпадает ли пароль
-    //в 6 уроке есть пример дз
-  },
-
-  async _generateHash(password: string, salt: string) {
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
   },
 
   async checkUserCredentials(
     loginOrEmail: string,
-    password: string,
+    password: string
   ): Promise<Result<WithId<UserDBType> | null>> {
     const user = await usersRepository.isUserExistByEmail(loginOrEmail);
     if (!user) {
@@ -46,7 +40,7 @@ export const authService = {
     }
     const isPasscorrect = await bcryptService.checkPassword(
       password,
-      user.passwordHash,
+      user.passwordHash
     );
     if (!isPasscorrect)
       return {
