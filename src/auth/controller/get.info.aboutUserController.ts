abd1d0709b445459c7b06b70f6144df2343aa895
is6ream <dnlils10@icloud.com>
2025-07-28 07:12:15 +0500

    прописал контроллер для получения информации о пользователе

---
+++ src/auth/controller/get.info.aboutUserController.ts
@@ -0,0 +1,17 @@
+import { HttpStatus } from "../../core/http-statuses";
+import { IdType } from "../../core/types/authorization/id";
+import { RequestWithUserId } from "../../core/types/requests/requests";
+import { usersQueryRepository } from "../../users/repositories/user.query.repository";
+import { Response } from "express";
+
+export async function getInfoAboutUserController(
+  req: RequestWithUserId<IdType>,
+  res: Response,
+) {
+  const userId = req.user?.id as string;
+
+  if (!userId) return res.status(HttpStatus.Unauthorized);
+  const me = await usersQueryRepository.findById(userId);
+
+  return res.status(HttpStatus.Ok).send(me);
+}
--- src/core/types/requests/requests.ts
+++ src/core/types/requests/requests.ts
@@ -6,7 +6,7 @@ export type RequestWithQuery<Q> = Request<{}, {}, {}, Q>;
 export type RequestWithParams<P> = Request<P>;
 export type RequestWithParamsAndBody<P, B> = Request<P, {}, B>;
 export type RequestWithParamsAndQuery<P, Q> = Request<P, {}, {}, Q>;
-export type RequsetWithParamsAndBodyAndUserId<P, B, U extends IdType> = Request<
+export type RequestWithParamsAndBodyAndUserId<P, B, U extends IdType> = Request<
   P,
   {},
   B,
