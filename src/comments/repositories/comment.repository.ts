3dd8459831d103165707f5a7c983f9b170b39605
is6ream <dnlils10@icloud.com>
2025-07-28 22:28:56 +0500

    прописал DAL comments

--- src/comments/repositories/comment.repository.ts
+++ src/comments/repositories/comment.repository.ts
@@ -1,10 +1,34 @@
-import { CommentDBType, CommentInputType } from "../types/commentsTypes";
+import { CommentInputType } from "../types/commentsTypes";
 import { commentsCollection } from "../../db/mongo.db";
-import { ObjectId, WithId } from "mongodb";
+import { ObjectId } from "mongodb";
 
 export const commentsRepository = {
   async create(newComment: CommentInputType): Promise<string> {
     const insertResult = await commentsCollection.insertOne(newComment);
     return insertResult.insertedId.toString();
+  },
+
+  async update(id: string, dto: CommentInputType): Promise<void | null> {
+    const updateResult = await commentsCollection.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      {
+        $set: {
+          content: dto.content,
+        },
+      }
+    );
+    if (updateResult.matchedCount < 1) {
+      return null;
+    }
+    return;
+  },
+
+  async delete(id: string): Promise<boolean> {
+    const deleteResult = await commentsCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
+    return deleteResult.deletedCount === 1;
   },
 };
