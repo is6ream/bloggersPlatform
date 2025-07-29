ba1e0c5a778c64b48043fcf12265f6d841eba4c9
is6ream <dnlils10@icloud.com>
2025-07-29 07:26:29 +0500

    прописал роут для создания комментов

---
+++ src/comments/routes/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { accessTokenGuard } from "../../core/guards/access.token.guard";
+import { contentValidator } from "./../../core/middlewares/postValidation/post-input-dto.validation";
+import { inputValidationResultMiddleware } from "../../core/middlewares/validation/input-validation-result.middleware";
+
+export const commentsRouter = Router();
+
+commentsRouter.post(
+  "/posts/:id/comments",
+  accessTokenGuard,
+  contentValidator,
+  inputValidationResultMiddleware
+);
