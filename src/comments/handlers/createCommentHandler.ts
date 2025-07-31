28431b1d0106f1b417dfd9732f954daa34190c29
is6ream <dnlils10@icloud.com>
2025-07-31 08:02:28 +0500

    прописал контроллер для создания коммента

--- src/comments/handlers/createCommentHandler.ts
+++ src/comments/handlers/createCommentHandler.ts
@@ -5,19 +5,29 @@ import { Response } from "express";
 import { commentsService } from "../application/comments.service";
 import { ContentType } from "../types/commentsTypes";
 import { commentsQueryRepository } from "../repositories/commentsQueryRepository";
+import { ResultStatus } from "../../core/result/resultCode";
 
 export async function createCommentHandler(
   req: RequestWithParamsAndBodyAndUserId<string, ContentType, IdType>,
-  res: Response
+  res: Response,
 ) {
-  const userId = req.user?.id as string;
-  const content = {
-    comment: req.body,
-    userId: userId,
-    postId: req.params,
-  };
-  if (!userId) res.sendStatus(HttpStatus.Unauthorized);
-  const commentId = await commentsService.createComment(content);
-  const dataForResponse = await commentsQueryRepository.findById(commentId);
-  res.status(HttpStatus.Created).send(dataForResponse);
+  try {
+    const userId = req.user?.id as string;
+    const content = {
+      comment: req.body,
+      userId: userId,
+      postId: req.params,
+    };
+    if (!userId) res.sendStatus(HttpStatus.Unauthorized);
+    const result = await commentsService.createComment(content);
+    if (result.status === ResultStatus.Success) {
+      const dataForResponse = await commentsQueryRepository.findById(
+        result.data!.commentId,
+      );
+      res.status(HttpStatus.Created).send(dataForResponse);
+    }
+  } catch (error: unknown) {
+    console.log(error);
+    res.sendStatus(HttpStatus.InternalServerError);
+  }
 }
