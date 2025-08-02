e35587c72ab1b394602aeaebc488945b2a2b967f
is6ream <dnlils10@icloud.com>
2025-08-02 19:17:04 +0500

    при реквесте на поиск всех комментов поле контент должно возвращаться как строка

--- src/core/paths.ts
+++ src/core/paths.ts
@@ -4,4 +4,4 @@ export const TESTING_PATH = "/hometask_06/api/testing";
 export const TESTING_HW_PATH = "/hometask_06/api/";
 export const AUTH_PATH = "/hometask_06/api/";
 export const USERS_PATH = "/hometask_06/api/users";
-export const COMMENTS_PATH = "hometask_06/api/comments";
+export const COMMENTS_PATH = "/hometask_06/api/comments";
