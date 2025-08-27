cfc00b4c75e9ba817d428621395af942943aa830
is6ream <dnlils10@icloud.com>
2025-08-27 07:29:10 +0500

    started working with integration tests

---
+++ src/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: "ts-jest", // чтобы работал TypeScript
+  testEnvironment: "node", 
+  testMatch: ["**/*.e2e-spec.ts", "**/*.test.ts"], // ищет только тесты
+};
+
+module.exports = config;
