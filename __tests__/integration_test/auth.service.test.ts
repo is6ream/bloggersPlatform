6c47360d8763bf60038ebbe2bb18e6873bb1ab3c
is6ream <dnlils10@icloud.com>
2025-08-29 07:47:59 +0500

    I stopped at the point where I started writing integration tests as in the repository

---
+++ __tests__/integration_test/auth.service.test.ts
@@ -0,0 +1,3 @@
+import { authService } from './../../src/auth/application/auth.service';
+import { emailAdapter } from './../../src/auth/adapters/nodemailer.service';
+import { MongoMemoryServer } from 'mongodb-memory-server';
