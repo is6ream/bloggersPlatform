c1784ec569c6f2df7a65b314012abc1642caad2d
is6ream <dnlils10@icloud.com>
2025-08-29 07:18:23 +0500

    rewrote email adapter to gmail

--- src/auth/adapters/nodemailer.service.ts
+++ src/auth/adapters/nodemailer.service.ts
@@ -1,28 +1,54 @@
 import nodemailer from "nodemailer";
 
-export const emailAdapter = { //переписать на гугл
+// export const emailAdapter = {
+//   //переписать на гугл
+//   async sendEmail(
+//     email: string,
+//     code: string,
+//     template: (code: string) => string
+//   ): Promise<boolean> {
+//     const transport = nodemailer.createTransport({
+//       host: "smtp.mail.ru",
+//       port: 465,
+//       secure: true, // TLS
+//       auth: {
+//         user: "testnodemailer12@mail.ru",
+//         pass: "KCKW7EgvI0XFc2zionaN",
+//       },
+//     });
+
+//     const info = transport.sendMail({
+//       from: '"Danil" <testnodemailer12@mail.ru>',
+//       to: email,
+//       subject: "Your registration code is here!",
+//       html: template(code),
+//     });
+
+//     return !!info;
+//   },
+// };
+
+export const emailAdapter = {
   async sendEmail(
     email: string,
     code: string,
     template: (code: string) => string
   ): Promise<boolean> {
     const transport = nodemailer.createTransport({
-      host: "smtp.mail.ru",
-      port: 465,
-      secure: true, // TLS
+      service: "gmail",
       auth: {
-        user: "testnodemailer12@mail.ru",
-        pass: "KCKW7EgvI0XFc2zionaN",
+        user: "d.ilyasovunibell@gmail.com",
+        pass: process.env.GOOGLE_APP_PASSWORD,
       },
     });
+    console.log(process.env.GOOGLE_APP_PASSWORD);
 
     const info = transport.sendMail({
-      from: '"Danil" <testnodemailer12@mail.ru>',
+      from: '"Danil" <d.ilyasovunibell@gmail.com>',
       to: email,
-      subject: "Your registration code is here!",
+      subject: "Your registration code is here",
       html: template(code),
     });
-
     return !!info;
   },
 };
