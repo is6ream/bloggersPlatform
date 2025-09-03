553d94e2e9d6b4599b05eda5a8598819be624f77
is6ream <dnlils10@icloud.com>
2025-09-03 07:31:25 +0500

    replaced the paths with the current ones

--- src/core/paths.ts
+++ src/core/paths.ts
@@ -1,8 +1,8 @@
-export const BLOGS_PATH = "/hometask_06/api/blogs";
-export const POSTS_PATH = "/hometask_06/api/posts";
-export const TESTING_PATH = "/hometask_06/api/testing";
-export const TESTING_HW_PATH = "/hometask_06/api/";
-export const AUTH_PATH = "/hometask_06/api/auth";
-export const USERS_PATH = "/hometask_06/api/users";
-export const COMMENTS_PATH = "/hometask_06/api/comments";
-export const EMAIL_PATH = "/hometask_06/api/email/";
+export const BLOGS_PATH = "/hometask_08/api/blogs";
+export const POSTS_PATH = "/hometask_08/api/posts";
+export const TESTING_PATH = "/hometask_08/api/testing";
+export const TESTING_HW_PATH = "/hometask_08/api/";
+export const AUTH_PATH = "/hometask_08/api/auth";
+export const USERS_PATH = "/hometask_08/api/users";
+export const COMMENTS_PATH = "/hometask_08/api/comments";
+export const EMAIL_PATH = "/hometask_08/api/email/";
