7ee4441b632969d4d64cb982a06ad9993248e953
is6ream <dnlils10@icloud.com>
2025-09-03 07:58:43 +0500

    register the logic for issuing refreshToken in the aith/login endpoint

--- src/auth/controller/refreshToken.controller.ts
+++ src/auth/controller/refreshToken.controller.ts
@@ -10,7 +10,7 @@ export async function refreshTokenController(
   req: RequestWithCookies,
   res: Response
 ) {
-  const refreshToken = req.cookies.refreshToken;
+  const refreshToken = req.cookies.refreshToken; //здесь мы получаем рефрешТокен, который изначально должен выдать эндпоинт логинизации
   const result = await genNewTokensService(refreshToken);
   if (result.status !== ResultStatus.Success) {
     //где должна осуществляться проверка валдиности токена?
