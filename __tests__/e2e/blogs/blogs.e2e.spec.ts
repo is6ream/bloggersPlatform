fcf45267654e3b266de3feba0fca4faaeb8f0d61
is6ream <dnlils10@icloud.com>
2025-09-09 22:16:36 +0500

    started writing tests for the blog branch

---
+++ __tests__/e2e/blogs/blogs.e2e.spec.ts
@@ -0,0 +1,29 @@
+import express, { Express } from "express";
+import { setupApp } from "../../../src/setup-app";
+import { db } from "../../../src/db/mongo.db";
+import request from "supertest";
+import { BLOGS_PATH } from "../../../src/core/paths";
+describe("Testing the blog branch", () => {
+  let app: Express;
+  beforeAll(async () => {
+    await db.runDB(
+      "mongodb+srv://admin:admin@cluster0.x2itf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    await db.drop();
+    const expressApp = express();
+    app = setupApp(expressApp);
+  });
+
+  afterAll(async () => {
+    await db.drop();
+    await db.stop();
+  });
+
+  it("should return blogs with paging", async () => {
+    const res = await request(app)
+      .get(BLOGS_PATH + "?sortBy=createdAt&pageNumber=1&pageSize=10")
+      .expect(200);
+
+    expect(res.body).toBeDefined;
+  });
+});
