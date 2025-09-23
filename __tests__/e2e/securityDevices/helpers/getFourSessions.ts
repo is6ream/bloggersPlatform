4447065c53679033e7a10119b680c80b2878b35c
is6ream <dnlils10@icloud.com>
2025-09-23 12:12:20 +0500

    Wrote a function to create four devices

--- __tests__/e2e/securityDevices/helpers/getFourSessions.ts
+++ __tests__/e2e/securityDevices/helpers/getFourSessions.ts
@@ -1,6 +1,6 @@
 import { Express } from "Express";
 import { registerUser } from "../../auth/helpers/registerUser";
-import {loginUser} from "../../auth/helpers/authUser";
+import { loginUser } from "../../auth/helpers/authUser";
 
 export type UserData = {
   login: string;
@@ -15,5 +15,31 @@ export async function getFourSessions(app: Express, userData: UserData) {
     password: userData.password,
   });
 
-  const firstUser = await loginUser(app, )
+  const firstUser = await loginUser(app);
+}
+
+export function getUserData(deviceName: string) {
+  let userAgent: string = "";
+
+  switch (deviceName) {
+    case "iphone":
+      userAgent = "iphone11";
+      break;
+    case "huawei D16":
+      userAgent = "huawei D16";
+      break;
+    case "Macbook":
+      userAgent = "Macbook";
+      break;
+    case "redmi 8":
+      userAgent = "redmi 8";
+      break;
+  }
+
+  return {
+    login: "test",
+    email: "test@mail.ru",
+    password: "test",
+    userAgent: userAgent,
+  };
 }
