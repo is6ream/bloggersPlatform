944a44f26a159feed484cd2ce3032fa44345442e
is6ream <dnlils10@icloud.com>
2025-10-25 13:07:01 +0500

    connected the project to the database

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import { setupApp } from "./setup-app";
 import { appConfig } from "./core/config/config";
-import { db } from "./db/mongo.db";
+// import { db } from "./db/mongo.db";
+import {db} from "./db/runDb";
 
 const bootStrap = async () => {
   const app = express();
   app.set("trust proxy", true);
   setupApp(app);
   const PORT = process.env.PORT || 3000;
-  await db.runDB(appConfig.MONGO_URL);
+  await db.runDb();
 
   app.listen(PORT, () => {
     console.log(`Example app listening on port: ${PORT}`);
