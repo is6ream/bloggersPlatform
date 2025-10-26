7ed588d7250dd8da0b5153c83e9c6a7c86dedad4
is6ream <dnlils10@icloud.com>
2025-10-26 11:51:40 +0500

    Changed the method for deleting a block via Mongoose

--- src/blogs/infrastructure/blogs.repository.ts
+++ src/blogs/infrastructure/blogs.repository.ts
@@ -1,34 +1,24 @@
 import { BlogInputDto, BlogViewModel } from "../types/blogs-types";
-import { blogCollection } from "../../db/mongo.db";
 import { ObjectId } from "mongodb";
 import { injectable } from "inversify";
 import { BlogModel } from "../types/mongoose";
 
 @injectable()
 export class BlogsRepository {
-  //нужно понять, куда сохраняются данные
   async create(newBlog: BlogInputDto): Promise<string> {
     const blogInstance = new BlogModel();
-    //как сделать save через репозиторий?
     blogInstance.name = newBlog.name;
     blogInstance.description = newBlog.description;
     blogInstance.websiteUrl = newBlog.websiteUrl;
     blogInstance.createdAt = newBlog.createdAt;
     blogInstance.isMembership = newBlog.isMembership;
 
-    console.log("Before save - isNew:", blogInstance.isNew); // true
-    console.log("Before save - isModified:", blogInstance.isModified()); // true
-
     await blogInstance.save();
-
-    console.log("After save - isNew:", blogInstance.isNew); // false
-    console.log("After save - _id:", blogInstance._id);
-
     return blogInstance._id.toString();
   }
 
   async findById(id: string): Promise<BlogViewModel | false> {
-    const blog = await BlogModel.findOne({ _id: new ObjectId(id) });
+    const blog = await BlogModel.findOne({ _id: new ObjectId(id) }).lean();
     if (!blog) {
       return false;
     }
@@ -51,25 +41,12 @@ export class BlogsRepository {
     blog.description = dto.description;
     blog.websiteUrl = dto.websiteUrl;
 
-    blog.save();
+    await blog.save();
     return true;
-    // await blogCollection.updateOne(
-    //   {
-    //     _id: new ObjectId(id),
-    //   },
-    //   {
-    //     $set: {
-    //       name: dto.name,
-    //       description: dto.description,
-    //       websiteUrl: dto.websiteUrl,
-    //     },
-    //   },
-    // );
-    // return updateResult.modifiedCount === 1;
   }
 
   async delete(id: string): Promise<boolean> {
-    const deleteResult = await blogCollection.deleteOne({
+    const deleteResult = await BlogModel.deleteOne({
       _id: new ObjectId(id),
     });
     return deleteResult.deletedCount === 1;
