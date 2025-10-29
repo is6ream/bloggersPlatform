58d30bd8a138c6e69e2c2652a34932d1a2d07874
is6ream <dnlils10@icloud.com>
2025-10-29 18:48:37 +0500

    refactored rtGuard

--- src/core/guards/refreshTokenGuard.ts
+++ src/core/guards/refreshTokenGuard.ts
@@ -12,34 +12,24 @@ export const refreshTokenGuard = async (
   res: Response,
   next: NextFunction,
 ) => {
-  //check
   const refreshToken = req.cookies?.refreshToken;
   if (!refreshToken) {
-    console.error("Refresh token in cookies not found");
     return res.sendStatus(HttpStatus.Unauthorized);
   }
   const payload: RefreshTokenPayload | null =
     await jwtService.verifyToken(refreshToken);
   if (!payload) {
-    console.log("Refresh token not verified");
     return res.sendStatus(HttpStatus.Unauthorized);
   }
-  console.log(
-    new Date(payload.iat * 1000).toISOString(), //нужно привести iat из payload в такой формат - "2025-10-29T02:31:56.677Z"
-    "iat check in rt guard",
-  );
+  const { deviceId, userId } = payload;
   const tokenCreationDate = new Date(payload.iat * 1000).toISOString();
-
-
   const activeSessionCheck = await sessionsRepository.isSessionExistByIat(
-    tokenCreationDate, payload.deviceId, //почему здесь формируется дата в момент проверки, а не дата, которая была записана в момент формирования сессии???
+    tokenCreationDate,
+    deviceId,
   );
-  //ошибка возникает из-за того, что при авторизации в бд сохраняется одна дата, а при проверке формируется другая
-  console.log(activeSessionCheck, "check active session");
   if (!activeSessionCheck) {
     return res.sendStatus(HttpStatus.Unauthorized);
   }
-  const { deviceId, userId } = payload; //достаем из payload id и передаем их далее
   req.deviceId = deviceId;
   req.userId = userId;
   next();
