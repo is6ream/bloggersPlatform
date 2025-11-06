8037388cf70e0279bf42e4f08233fe45f34812eb
is6ream <dnlils10@icloud.com>
2025-11-06 12:36:04 +0500

    updated the main path in the application

--- src/core/paths.ts
+++ src/core/paths.ts
@@ -1,8 +1,8 @@
-export const BLOGS_PATH = "/hometask_10/api/blogs";
-export const POSTS_PATH = "/hometask_10/api/posts";
-export const TESTING_PATH = "/hometask_10/api/testing";
-export const TESTING_HW_PATH = "/hometask_10/api/";
-export const AUTH_PATH = "/hometask_10/api/auth";
-export const USERS_PATH = "/hometask_10/api/users";
-export const COMMENTS_PATH = "/hometask_10/api/comments";
-export const SECURITY_DEVICES_PATH = "/hometask_10/api/security/devices";
+export const BLOGS_PATH = "/hometask_11/api/blogs";
+export const POSTS_PATH = "/hometask_11/api/posts";
+export const TESTING_PATH = "/hometask_11/api/testing";
+export const TESTING_HW_PATH = "/hometask_11/api/";
+export const AUTH_PATH = "/hometask_11/api/auth";
+export const USERS_PATH = "/hometask_11/api/users";
+export const COMMENTS_PATH = "/hometask_11/api/comments";
+export const SECURITY_DEVICES_PATH = "/hometask_11/api/security/devices";
