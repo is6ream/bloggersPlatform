a89453358332f9bb52aa9ec6b980cf320915dc85
is6ream <dnlils10@icloud.com>
2025-11-24 07:52:35 +0500

    reafctoring
    changed the routes

--- src/core/paths.ts
+++ src/core/paths.ts
@@ -1,8 +1,8 @@
-export const BLOGS_PATH = "/hometask_11/api/blogs";
-export const POSTS_PATH = "/hometask_11/api/posts";
-export const TESTING_PATH = "/hometask_11/api/testing";
-export const TESTING_HW_PATH = "/hometask_11/api/";
-export const AUTH_PATH = "/hometask_11/api/auth";
-export const USERS_PATH = "/hometask_11/api/users";
-export const COMMENTS_PATH = "/hometask_11/api/comments";
-export const SECURITY_DEVICES_PATH = "/hometask_11/api/security/devices";
+export const BLOGS_PATH = "/hometask_12/api/blogs";
+export const POSTS_PATH = "/hometask_12/api/posts";
+export const TESTING_PATH = "/hometask_12/api/testing";
+export const TESTING_HW_PATH = "/hometask_12/api/";
+export const AUTH_PATH = "/hometask_12/api/auth";
+export const USERS_PATH = "/hometask_12/api/users";
+export const COMMENTS_PATH = "/hometask_12/api/comments";
+export const SECURITY_DEVICES_PATH = "/hometask_12/api/security/devices";
